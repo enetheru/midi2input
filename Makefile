12acbf6cd119496c3d57c44cf87d23377fa06d6c
Samuel Nicholas <nicholas.samuel@gmail.com>
2015-11-05 18:06:17 +1030

    load configuration from priority list of locations

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ LDFLAGS=-lX11 -lXtst -ljack
 CFLAGS=-Wall
 
 mid2key: mid2key.c
-	cc $(CFLAGS) $^ -o $@ $(LDFLAGS)
+	g++ $(CFLAGS) $^ -o $@ $(LDFLAGS)
 clean: 
 	rm -f mid2key
--- mid2key.c
+++ mid2key.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
 #include <wordexp.h>
 #include <unistd.h>
-
 
 #include <jack/jack.h> 
 #include <jack/midiport.h> 
@@ -14,19 +14,69 @@
 Display* xdp;
 jack_client_t *client;
 jack_port_t *input_port;
-KeyCode map[128];
+KeyCode map[ 128 ];
+
+FILE *
+load_config( char *filename )
+{
+	//TODO use a queue of filepaths and c++ streams
+	// load configuration from a priority list of locations
+	// * specified from the command line
+	// * current directory ./.midi2input.cfg
+	// * configuration folder $HOME/.config/
+	// * home directory $HOME/
+	FILE *tFile;
+	char filepath[ 1024 ];
+
+	// cmd line
+	tFile = fopen( filename, "r" );
+	if(  tFile != NULL ){
+		printf( "cfg: %s\n", filename );
+		return tFile;
+	}
+
+	// current working directory
+	char cwd[ 1024 ];
+	getcwd( cwd, 1024 );
+	
+	sprintf( filepath, "%s/.midi2input.cfg", cwd );
+
+	tFile = fopen( filepath, "r" );
+	if(  tFile != NULL ){
+		printf( "cfg: %s\n", filepath );
+		return tFile;
+	}
+	// configuration folder ~/.config/
+	sprintf( filepath, "%s/.config/midi2key.cfg", getenv( "HOME" ) );
+
+	tFile = fopen( filepath, "r" );
+	if(  tFile != NULL ){
+		printf( "cfg: %s\n", filepath );
+		return tFile;
+	}
+	
+	// configuration folder ~/.config/
+	sprintf( filepath, "%s/.midi2key.cfg", getenv( "HOME" ) );
+
+	tFile = fopen( filepath, "r" );
+	if(  tFile != NULL ){
+		printf( "cfg: %s\n", filepath );
+		return tFile;
+	}
+
+	return NULL;
+}
 
 int
 process( jack_nframes_t nframes, void *arg )
 {
-    int i;
     int temp;
     void* port_buf = jack_port_get_buffer( input_port, nframes );
     jack_midi_event_t in_event;
     jack_nframes_t event_count = jack_midi_get_event_count( port_buf );
     
     if( event_count > 0 ){
-        for( i = 0; i < event_count; i++ ){
+        for(uint32_t i = 0; i < event_count; i++ ){
             jack_midi_event_get( &in_event, port_buf, i );
 
             if( in_event.buffer[ 0 ] == 0x90 ){
@@ -64,10 +114,10 @@ main( int argc, char** argv )
 {
 
     /*usage*/
-    if( argc != 2 ){
+    if( argc == 2 && strcmp(argv[1], "-h") == 0 ){
         fprintf( stderr,
             "Usage: %s [config]\n"
-            "Defaults to ~/.mid2key if config is unspecified.\n"
+            "Defaults to ~/.config/midi2input.cfg if config is unspecified.\n"
             "Configuration example:\n"
             "#set note #32 to spacebar\n"
             "32=space\n"
@@ -109,22 +159,12 @@ main( int argc, char** argv )
     char buffer[128];
     char* keysym;
     int temp;
-    wordexp_t file_expanded;
 
     /*config*/
-    const char* file;
-    if( argc == 2 ){
-        file = argv[1];
-    }
-    else {
-        file = "~/.mid2key";
-    }
-
-    wordexp( file, &file_expanded, 0 );
     memset( map, 0, sizeof( map ) );
-
-    if(! (config = fopen( file_expanded.we_wordv[ 0 ], "r" )) ){
-        fprintf( stderr, "Unable to open configuration file '%s'\nUsing null mapping.\n", file );
+	
+    if(! (config = load_config( argv[ 1 ] )) ){
+        fprintf( stderr, "Unable to open configuration file\nUsing null mapping.\n" );
     }
     else {
         while( fgets( buffer, 128, config ) ){
