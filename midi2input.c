af1790912d6d4960550bb01cb8c2776182f739d6
Samuel Nicholas <nicholas.samuel@gmail.com>
2015-11-21 09:49:07 +1030

    added rudimentary x window detection

--- midi2input.c
+++ midi2input.c
@@ -58,14 +58,43 @@ load_config(  std::string name )
 int
 process( jack_nframes_t nframes, void *arg )
 {
+	//detect window
+	Window w;
+	static Window w_current;
+	int revert_to;
+	XGetInputFocus( xdp, &w, &revert_to );
+	if( w != None && w != w_current){
+		w_current = w;
+		std::cout << "window ID: " << w_current;
+
+		int format;
+		unsigned long remain, len;
+		unsigned char *list;
+
+		//TODO Xfree() ?
+		Atom prop = XInternAtom( xdp, "WM_CLASS", False ), type;
+		if( XGetWindowProperty( xdp, w, prop, 0, 1024, False, AnyPropertyType,
+					&type, &format, &len, &remain, &list ) == Success ){
+			if( format == 8 ) std::cout << "\tWM_CLASS: " << list;
+		}
+		prop = XInternAtom( xdp, "WM_NAME", False );
+		if( XGetWindowProperty( xdp, w, prop, 0, 1024, False, AnyPropertyType,
+					&type, &format, &len, &remain, &list ) == Success ){
+			if( format == 8 ) std::cout << "\tWM_NAME: " << list;
+		}
+		std::cout << std::endl;
+
+	}
+
+	//process midi events
 	KeyCode temp;
-    void* port_buf = jack_port_get_buffer( input_port, nframes );
-    jack_midi_event_t in_event;
-    jack_nframes_t event_count = jack_midi_get_event_count( port_buf );
-    
-    if( event_count > 0 ){
-        for(uint32_t i = 0; i < event_count; i++ ){
-            jack_midi_event_get( &in_event, port_buf, i );
+	void* port_buf = jack_port_get_buffer( input_port, nframes );
+	jack_midi_event_t in_event;
+	jack_nframes_t event_count = jack_midi_get_event_count( port_buf );
+	
+	if( event_count > 0 ){
+		for(uint32_t i = 0; i < event_count; i++ ){
+			jack_midi_event_get( &in_event, port_buf, i );
 
 			if( (in_event.buffer[1] <= 127) && (in_event.buffer[2]) ){
 				if( (temp = map[ in_event.buffer[ 1 ] ]) ){
@@ -83,87 +112,87 @@ process( jack_nframes_t nframes, void *arg )
 					<< std::endl;
 			}
 
-        }
-    }
+		}
+	}
 
-    return 0;
+	return 0;
 }
 
 void
 jack_shutdown( void *arg )
 {
-    exit( 1 );
+	exit( 1 );
 }
 
 int
 main( int argc, char** argv )
 {
 
-    /*usage*/
-    if( argc == 2 && std::string( argv[ 1 ] ).compare( "-h" ) == 0 ){
+	/*usage*/
+	if( argc == 2 && std::string( argv[ 1 ] ).compare( "-h" ) == 0 ){
 		std::cerr << "Usage: " << *argv << " [config]\n"
-            "Defaults to ~/.config/midi2input.cfg if config is unspecified.\n"
-            "Configuration example:\n"
-            "#set note #32 to spacebar\n"
-            "32=space\n"
-            "#set note #33 to keycode 67\n"
-            "33=67\n"
-            "#leave note #34 unset\n"
-            "34=\n"
-            "#also set note 35 to space\n"
-            "35=space\n";
-        
-        return 1;
-    }
+			"Defaults to ~/.config/midi2input.cfg if config is unspecified.\n"
+			"Configuration example:\n"
+			"#set note #32 to spacebar\n"
+			"32=space\n"
+			"#set note #33 to keycode 67\n"
+			"33=67\n"
+			"#leave note #34 unset\n"
+			"34=\n"
+			"#also set note 35 to space\n"
+			"35=space\n";
+		
+		return 1;
+	}
 
-    /*display*/
-    if(! (xdp = XOpenDisplay( getenv( "DISPLAY" ) )) ){
+	/*display*/
+	if(! (xdp = XOpenDisplay( getenv( "DISPLAY" ) )) ){
 		std::cerr << "Unable to open X display\n";
-        return 2;
-    }
+		return 2;
+	}
 
-    /*jack*/
-    if( (client = jack_client_open( "midi2input", JackNullOption, NULL )) == 0 ){
+	/*jack*/
+	if( (client = jack_client_open( "midi2input", JackNullOption, NULL )) == 0 ){
 		std::cerr << "jack server not running?\n";
-        return 1;
-    }
+		return 1;
+	}
 
-    jack_set_process_callback( client, process, 0 );
-    jack_on_shutdown( client, jack_shutdown, 0 );
+	jack_set_process_callback( client, process, 0 );
+	jack_on_shutdown( client, jack_shutdown, 0 );
 
-    input_port = jack_port_register( client, "midi_in", JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0 );
+	input_port = jack_port_register( client, "midi_in", JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0 );
 
-    if( jack_activate( client ) ){
+	if( jack_activate( client ) ){
 		std::cerr << "cannot activate client\n";
-        return 1;
-    }
+		return 1;
+	}
 
-    /*defines*/
+	/*defines*/
 	std::ifstream config;
 	std::string cfg_fn;
 	std::string cell;
 	std::stringstream line;
 	std::vector<std::string> tokens;
-    int temp;
+	int temp;
 	int count = 0;
 
-    /*config*/
+	/*config*/
 	cfg_fn = load_config( argv[ 1 ] );
-    if( cfg_fn.empty() ){
+	if( cfg_fn.empty() ){
 		std::cerr << "Unable to open configuration file\n"
 			"Using null mapping.\n";
-    }
-    else {
+	}
+	else {
 		config.open( cfg_fn.c_str(), std::ifstream::in );
 
 		while( std::getline( config, cell ) ){
 			++count;
 			if( cell[ 0 ] == '#' ) continue;
-        	line.clear();
-        	line.str( cell );
+			line.clear();
+			line.str( cell );
 
-        	tokens.clear();
-        	while( std::getline( line, cell, '=' ) ) tokens.push_back( cell );
+			tokens.clear();
+			while( std::getline( line, cell, '=' ) ) tokens.push_back( cell );
 			if( tokens.size() != 2 ) {
 				std::cerr << "parse error on line " << count << std::endl;
 				continue;
@@ -179,19 +208,19 @@ main( int argc, char** argv )
 			// * sliders & knobs with double precision
 			// * continuous wheels
  
-    	}
+		}
 
-        config.close();
-    }
+		config.close();
+	}
    
-    while(1)
-    {
-        sleep(1);
-    }
-    jack_client_close(client);
-    exit (0);
+	while(1)
+	{
+		sleep(1);
+	}
+	jack_client_close(client);
+	exit (0);
 
-    return 0;
+	return 0;
 }
 
 
