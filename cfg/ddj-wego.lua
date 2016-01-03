cde214e2fd119acc9b21293cf2ffd5eb358cf56a
Enethery <nicholas.samuel@gmail.com>
2016-01-03 18:41:02 +1030

    event mapping for fullscreen and maximize toggle

--- cfg/ddj-wego.lua
+++ cfg/ddj-wego.lua
@@ -21,6 +21,126 @@ XK_Next                        = 0xff56  --/* Next */
 XK_Page_Down                   = 0xff56
 XK_End                         = 0xff57  --/* EOL */
 XK_Begin                       = 0xff58  --/* BOL */
+XK_F1                          = 0xffbe
+XK_F2                          = 0xffbf
+XK_F3                          = 0xffc0
+XK_F4                          = 0xffc1
+XK_F5                          = 0xffc2
+XK_F6                          = 0xffc3
+XK_F7                          = 0xffc4
+XK_F8                          = 0xffc5
+XK_F9                          = 0xffc6
+XK_F10                         = 0xffc7
+XK_F11                         = 0xffc8
+XK_L1                          = 0xffc8
+XK_F12                         = 0xffc9
+XK_L2                          = 0xffc9
+XK_F13                         = 0xffca
+XK_L3                          = 0xffca
+XK_F14                         = 0xffcb
+XK_L4                          = 0xffcb
+XK_F15                         = 0xffcc
+XK_L5                          = 0xffcc
+XK_F16                         = 0xffcd
+XK_L6                          = 0xffcd
+XK_F17                         = 0xffce
+XK_L7                          = 0xffce
+XK_F18                         = 0xffcf
+XK_L8                          = 0xffcf
+XK_F19                         = 0xffd0
+XK_L9                          = 0xffd0
+XK_F20                         = 0xffd1
+XK_L10                         = 0xffd1
+XK_F21                         = 0xffd2
+XK_R1                          = 0xffd2
+XK_F22                         = 0xffd3
+XK_R2                          = 0xffd3
+XK_F23                         = 0xffd4
+XK_R3                          = 0xffd4
+XK_F24                         = 0xffd5
+XK_R4                          = 0xffd5
+XK_F25                         = 0xffd6
+XK_R5                          = 0xffd6
+XK_F26                         = 0xffd7
+XK_R6                          = 0xffd7
+XK_F27                         = 0xffd8
+XK_R7                          = 0xffd8
+XK_F28                         = 0xffd9
+XK_R8                          = 0xffd9
+XK_F29                         = 0xffda
+XK_R9                          = 0xffda
+XK_F30                         = 0xffdb
+XK_R10                         = 0xffdb
+XK_F31                         = 0xffdc
+XK_R11                         = 0xffdc
+XK_F32                         = 0xffdd
+XK_R12                         = 0xffdd
+XK_F33                         = 0xffde
+XK_R13                         = 0xffde
+XK_F34                         = 0xffdf
+XK_R14                         = 0xffdf
+XK_F35                         = 0xffe0
+XK_R15                         = 0xffe0
+XK_Shift_L                     = 0xffe1  --/* Left shift */
+XK_Shift_R                     = 0xffe2  --/* Right shift */
+XK_Control_L                   = 0xffe3  --/* Left control */
+XK_Control_R                   = 0xffe4  --/* Right control */
+XK_Caps_Lock                   = 0xffe5  --/* Caps lock */
+XK_Shift_Lock                  = 0xffe6  --/* Shift lock */
+XK_Meta_L                      = 0xffe7  --/* Left meta */
+XK_Meta_R                      = 0xffe8  --/* Right meta */
+XK_Alt_L                       = 0xffe9  --/* Left alt */
+XK_Alt_R                       = 0xffea  --/* Right alt */
+XK_Super_L                     = 0xffeb  --/* Left super */
+XK_Super_R                     = 0xffec  --/* Right super */
+XK_Hyper_L                     = 0xffed  --/* Left hyper */
+XK_Hyper_R                     = 0xffee  --/* Right hyper */
+XK_space                       = 0x0020  --/* U+0020 SPACE */
+XK_exclam                      = 0x0021  --/* U+0021 EXCLAMATION MARK */
+XK_quotedbl                    = 0x0022  --/* U+0022 QUOTATION MARK */
+XK_numbersign                  = 0x0023  --/* U+0023 NUMBER SIGN */
+XK_dollar                      = 0x0024  --/* U+0024 DOLLAR SIGN */
+XK_percent                     = 0x0025  --/* U+0025 PERCENT SIGN */
+XK_ampersand                   = 0x0026  --/* U+0026 AMPERSAND */
+XK_apostrophe                  = 0x0027  --/* U+0027 APOSTROPHE */
+XK_quoteright                  = 0x0027  --/* deprecated */
+XK_parenleft                   = 0x0028  --/* U+0028 LEFT PARENTHESIS */
+XK_parenright                  = 0x0029  --/* U+0029 RIGHT PARENTHESIS */
+XK_asterisk                    = 0x002a  --/* U+002A ASTERISK */
+XK_plus                        = 0x002b  --/* U+002B PLUS SIGN */
+XK_comma                       = 0x002c  --/* U+002C COMMA */
+XK_minus                       = 0x002d  --/* U+002D HYPHEN-MINUS */
+XK_period                      = 0x002e  --/* U+002E FULL STOP */
+XK_slash                       = 0x002f  --/* U+002F SOLIDUS */
+XK_0                           = 0x0030  --/* U+0030 DIGIT ZERO */
+XK_1                           = 0x0031  --/* U+0031 DIGIT ONE */
+XK_2                           = 0x0032  --/* U+0032 DIGIT TWO */
+XK_3                           = 0x0033  --/* U+0033 DIGIT THREE */
+XK_4                           = 0x0034  --/* U+0034 DIGIT FOUR */
+XK_5                           = 0x0035  --/* U+0035 DIGIT FIVE */
+XK_6                           = 0x0036  --/* U+0036 DIGIT SIX */
+XK_7                           = 0x0037  --/* U+0037 DIGIT SEVEN */
+XK_8                           = 0x0038  --/* U+0038 DIGIT EIGHT */
+XK_9                           = 0x0039  --/* U+0039 DIGIT NINE */
+XK_colon                       = 0x003a  --/* U+003A COLON */
+XK_semicolon                   = 0x003b  --/* U+003B SEMICOLON */
+XK_less                        = 0x003c  --/* U+003C LESS-THAN SIGN */
+XK_equal                       = 0x003d  --/* U+003D EQUALS SIGN */
+XK_greater                     = 0x003e  --/* U+003E GREATER-THAN SIGN */
+XK_question                    = 0x003f  --/* U+003F QUESTION MARK */
+XK_at                          = 0x0040  --/* U+0040 COMMERCIAL AT */
+XK_A                           = 0x0041  --/* U+0041 LATIN CAPITAL LETTER A */
+XK_B                           = 0x0042  --/* U+0042 LATIN CAPITAL LETTER B */
+XK_C                           = 0x0043  --/* U+0043 LATIN CAPITAL LETTER C */
+XK_D                           = 0x0044  --/* U+0044 LATIN CAPITAL LETTER D */
+XK_E                           = 0x0045  --/* U+0045 LATIN CAPITAL LETTER E */
+XK_F                           = 0x0046  --/* U+0046 LATIN CAPITAL LETTER F */
+XK_G                           = 0x0047  --/* U+0047 LATIN CAPITAL LETTER G */
+XK_H                           = 0x0048  --/* U+0048 LATIN CAPITAL LETTER H */
+XK_I                           = 0x0049  --/* U+0049 LATIN CAPITAL LETTER I */
+XK_J                           = 0x004a  --/* U+004A LATIN CAPITAL LETTER J */
+XK_K                           = 0x004b  --/* U+004B LATIN CAPITAL LETTER K */
+XK_L                           = 0x004c  --/* U+004C LATIN CAPITAL LETTER L */
 XK_Shift_L                     = 0xffe1  --/* Left shift */
 XK_Shift_R                     = 0xffe2  --/* Right shift */
 XK_Control_L                   = 0xffe3  --/* Left control */
@@ -144,15 +264,13 @@ eg. { {0x9, 0-15, 0-127, 0-127}, {0x00, 'b'} }
     ]]--
 function alt_tab()
 	keydown( XK_Alt_L )
-	keydown( XK_Tab )
-	keyup( XK_Tab )
+	keypress( XK_Tab )
 end
 
 function alt_shift_tab()
 	keydown( XK_Alt_L )
 	keydown( XK_Shift_L )
-	keydown( XK_Tab )
-	keyup( XK_Tab )
+	keypress( XK_Tab )
 	keyup( XK_Shift_L )
 end
 
@@ -160,15 +278,22 @@ function alt_up()
 	keyup( XK_Alt_L )
 end
 
+function alt_f10()
+	keydown( XK_Alt_L )
+	keypress( XK_F10 )
+	keyup( XK_Alt_L )
+end
+
 default = {
-	{ { 0xB0,  7, 64,   1 }, { alt_tab } },
-	{ { 0xB0,  7, 64, 127 }, { alt_shift_tab } },
-	{ { 0x90,  7, 65, 127 }, { alt_up } },
-	{ { 0xB0,  1, 19,   1 }, { keypress, XK_Down } },
-	{ { 0xB0,  1, 19, 127 }, { keypress, XK_Up } },
-	{ { 0xB0,  2, 19,   1 }, { keypress, XK_Right } },
-	{ { 0xB0,  2, 19, 127 }, { keypress, XK_Left } },
-	{ { 0x90, -1, 20,  -1 }, { keypress, XK_Return } },
+	{ { 0xB,  7, 64,   1 }, { alt_tab } },
+	{ { 0xB,  7, 64, 127 }, { alt_shift_tab } },
+	{ { 0x9,  7, 65, 127 }, { alt_up } },
+	{ { 0xB,  1, 19,   1 }, { keypress, XK_Down } },
+	{ { 0xB,  1, 19, 127 }, { keypress, XK_Up } },
+	{ { 0xB,  2, 19,   1 }, { keypress, XK_Right } },
+	{ { 0xB,  2, 19, 127 }, { keypress, XK_Left } },
+	{ { 0x9, -1, 20,  -1 }, { keypress, XK_Return } },
+	{ { 0x9,  5, 67,  127}, { alt_f10 } }, 
 }
 
 function banshee_shift_tab( )
@@ -204,9 +329,10 @@ function vlc_skip( midi_in )
 end
 
 vlc = {
-	{ { 0x90, -1, 11, 127}, { keypress, XK_space } },
-	{ { 0xB0, -1,  0, -1}, { vlc_speed } },
-	{ { 0xB0, -1, 34, -1}, { vlc_skip } },
+	{ { 0x9, -1, 11, 127}, { keypress, XK_space } },
+	{ { 0xB, -1,  0, -1 }, { vlc_speed } },
+	{ { 0xB, -1, 34, -1 }, { vlc_skip } },
+	{ { 0x9,  5, 68, 127}, { keypress, XK_f } },
 }
 
 applications = {
