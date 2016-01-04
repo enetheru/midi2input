f0b7c1301cbe3bcdcbf9da34042e3894f6819d5e
Samuel Nicholas <nicholas.samuel@gmail.com>
2016-01-04 18:39:28 +1030

    fix event tables to use 0xnn instead 0xn

--- cfg/ddj-wego.lua
+++ cfg/ddj-wego.lua
@@ -285,15 +285,15 @@ function alt_f10()
 end
 
 default = {
-	{ { 0xB,  7, 64,   1 }, { alt_tab } },
-	{ { 0xB,  7, 64, 127 }, { alt_shift_tab } },
-	{ { 0x9,  7, 65, 127 }, { alt_up } },
-	{ { 0xB,  1, 19,   1 }, { keypress, XK_Down } },
-	{ { 0xB,  1, 19, 127 }, { keypress, XK_Up } },
-	{ { 0xB,  2, 19,   1 }, { keypress, XK_Right } },
-	{ { 0xB,  2, 19, 127 }, { keypress, XK_Left } },
-	{ { 0x9, -1, 20,  -1 }, { keypress, XK_Return } },
-	{ { 0x9,  5, 67,  127}, { alt_f10 } }, 
+	{ { 0xB0,  7, 64,   1 }, { alt_tab } },
+	{ { 0xB0,  7, 64, 127 }, { alt_shift_tab } },
+	{ { 0x90,  7, 65, 127 }, { alt_up } },
+	{ { 0xB0,  1, 19,   1 }, { keypress, XK_Down } },
+	{ { 0xB0,  1, 19, 127 }, { keypress, XK_Up } },
+	{ { 0xB0,  2, 19,   1 }, { keypress, XK_Right } },
+	{ { 0xB0,  2, 19, 127 }, { keypress, XK_Left } },
+	{ { 0x90, -1, 20,  -1 }, { keypress, XK_Return } },
+	{ { 0x90,  5, 67,  127}, { alt_f10 } }, 
 }
 
 function banshee_shift_tab( )
@@ -329,10 +329,10 @@ function vlc_skip( midi_in )
 end
 
 vlc = {
-	{ { 0x9, -1, 11, 127}, { keypress, XK_space } },
-	{ { 0xB, -1,  0, -1 }, { vlc_speed } },
-	{ { 0xB, -1, 34, -1 }, { vlc_skip } },
-	{ { 0x9,  5, 68, 127}, { keypress, XK_f } },
+	{ { 0x90, -1, 11, 127}, { keypress, XK_space } },
+	{ { 0xB0, -1,  0, -1 }, { vlc_speed } },
+	{ { 0xB0, -1, 34, -1 }, { vlc_skip } },
+	{ { 0x90,  5, 68, 127}, { keypress, XK_f } },
 }
 
 applications = {
