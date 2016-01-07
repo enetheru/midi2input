68cb289d59af4ba115db9dfa3c72f16d20bbfb48
Samuel Nicholas <nicholas.samuel@gmail.com>
2016-01-07 11:10:42 +0000

    beginning of unifying docs

---
+++ doc/docs.cmake
@@ -0,0 +1,14 @@
+set( DOC_TITLE "midi2input"
+    CACHE STRING "")
+
+set( DOC_TITLE_LONG
+    "midi2input - Turns a midi device into a keyboard/mouse controller"
+    CACHE STRING "")
+
+set( DOC_DESCRIPTION
+    "This program takes midi events from jackd and translates them to input events.
+    Configuration is written in Lua it doesnt matter if i break spaces with this so long as it gets done right"
+    CACHE STRING "")
+
+set( DOC_AUTHORS "Samuel Nicholas <nicholas.samuel@gmail.com>"
+    CACHE STRING "")
