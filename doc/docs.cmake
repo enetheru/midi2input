dc80d4b2550816a28cf07fe720c4903d66c1202e
Samuel Nicholas <nicholas.samuel@gmail.com>
2016-01-08 20:51:41 +1030

    solved multi-line string cmake cache error, and whitespace
    cmake lets you use multi-line strings as of some version, but the cmake
    cache doesnt like them, so instead of caching the documentation strings
    i'm simply including them from docs.cmake when required.

--- doc/docs.cmake
+++ doc/docs.cmake
@@ -1,15 +1,14 @@
-set( DOC_TITLE "midi2input"
-    CACHE STRING "")
+# NOTE: cant cache multi line strings in cmake, so just include this file
+# whenever you need these strings
 
-set( DOC_TITLE_LONG
-    "midi2input - Turns a midi device into a keyboard/mouse controller"
-    CACHE STRING "")
+set( DOC_TITLE "midi2input" )
+set( DOC_DESCRIPTION_SHORT
+"Turns a midi device into a keyboard/mouse controller" )
 
 set( DOC_DESCRIPTION
-    "This program takes midi events from jackd and translates them to input "
-	"events. Configuration is written in Lua it doesnt matter if i break "
-    "spaces with this so long as it gets done right"
-    CACHE STRING "")
+"This program takes midi events from jackd and translates them to input events.
+Configuration is written in Lua it doesnt matter if i break spaces with this so
+long as it gets done right" )
 
-set( DOC_AUTHORS "Samuel Nicholas <nicholas.samuel@gmail.com>"
-    CACHE STRING "")
+set( DOC_AUTHORS "Samuel Nicholas <nicholas.samuel@gmail.com>" )
+
